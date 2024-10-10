6c57eec3cbdf4aa874208e4be593fcbdba0ac66d
lukezhao <lukezhao@stanford.edu>
2024-10-10 00:20:47 -0700

    initial

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 /**
  * You don't need to edit this file, but you can if you want!
  * Almost of the work for this assignment is in Header.js, Body.js, and Footer.js.
- * 
+ *
  * If you're doing the light/dark mode toggle extension, you will need to make
  * a couple tweaks here.
  */
 
 import { useCallback } from "react";
-import { StyleSheet, View, StatusBar } from "react-native";
+import { StyleSheet, View, StatusBar, SafeAreaView, Text } from "react-native";
 
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
@@ -38,11 +38,22 @@ export default function App() {
   /* ^Don't mind/edit this one either unless you decide to do the dark theme one, in that case, you will have to change it accordingly */
 
   return (
-    <View style={styles.container} onLayout={onLayoutRootView}>
-      <Header />
-      <Body />
-      <Footer />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* Header Section */}
+      <View style={styles.header}>
+        <Text>Header</Text>
+      </View>
+
+      {/* Body Section */}
+      <View style={styles.body}>
+        <Text>Body</Text>
+      </View>
+
+      {/* Footer Section */}
+      <View style={styles.footer}>
+        <Text>Footer</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -53,5 +64,27 @@ const styles = StyleSheet.create({
     backgroundColor: Themes.light.bg,
     alignItems: "center",
     paddingHorizontal: 16,
+  },
+  header: {
+    height: 60, // Fixed height for header
+    backgroundColor: "red", // For visualization
+    justifyContent: "center", // Center text vertically
+    alignItems: "center", // Center text horizontally
+  },
+  body: {
+    flex: 1, // Take up the remaining space
+    backgroundColor: "blue", // For visualization
+    justifyContent: "center", // Center text vertically
+    alignItems: "center", // Center text horizontally
+  },
+  footer: {
+    height: 60, // Fixed height for footer
+    backgroundColor: "green", // For visualization
+    justifyContent: "center", // Center text vertically
+    alignItems: "center", // Center text horizontally
+  },
+  text: {
+    fontSize: 20,
+    color: "#fff",
   },
 });
